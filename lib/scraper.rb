41083ce2e3fe1938a09c89676de3df2862d0acd4
danielpdaniel <artofjquinn@gmail.com>
2022-12-23 17:22:08 -0500

    commit tree 77724a11734b1d19fc35643259d406b7b5370ee1

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+# p doc.css(".heading-primary").text.strip
+courses = doc.css(".heading-20-semibold")
+courses.each do |course|
+    puts course.text.strip
+end
